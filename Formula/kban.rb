ae4a8c4f840351faa45a0a0b3ade3a2c33b83f40
David Pellerin <david@davidpellerin.com>
2026-03-06 13:20:02 -0500

    bump formula to v1.7.0

--- Formula/kban.rb
+++ Formula/kban.rb
@@ -1,8 +1,8 @@
 class Kban < Formula
   desc "Simple filesystem-based kanban board for Claude Code agents"
   homepage "https://github.com/davidpellerin/homebrew-kban"
-  url "https://github.com/davidpellerin/homebrew-kban/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "01f258b8bcf3ba86a586235d1c75861af7ef4576e848643a42691326e57fb122"
+  url "https://github.com/davidpellerin/homebrew-kban/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "06f9794f36e664a4854ddf2533af41fe1ddf01ccae5eb16043e07c96ec6c853e"
   license "MIT"
 
   def install
